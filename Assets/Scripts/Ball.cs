50cc0ae45ddc374572129c30d2ebc90736126c30
Can Kayacan <can.kayacan@zenjob.com>
2022-06-06 03:55:34 +0200

    Reset ball after collision.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,16 +1,26 @@
+using System;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    private int vacuumForce = 300;
+    private const int VacuumForce = 300;
+
     private Vector3? shootDestination;
+    private Rigidbody ballRigidbody;
+    private Vector3 initialPosition;
     
     public void Shoot(Vector3 force, Vector3 destination)
     {
+        ballRigidbody = GetComponent<Rigidbody>();
         shootDestination = destination;
         GetComponent<Rigidbody>().AddForce(force);
     }
-    
+
+    private void Start()
+    {
+        initialPosition = transform.position;
+    }
+
     private void Update()
     {
         if (shootDestination == null)
@@ -19,14 +29,20 @@ public class Ball : MonoBehaviour
         }
 
         var position = transform.position;
-        var force = -1 * (position - shootDestination.Value) * vacuumForce * Time.deltaTime;
-        // Debug.Log($"Update {force}");
-        GetComponent<Rigidbody>().AddForce(force);
+        var force = (position - shootDestination.Value) * (-1 * VacuumForce * Time.deltaTime);
+        ballRigidbody.AddForce(force);
         Debug.DrawLine(shootDestination.Value, position);
     }
     
     void OnCollisionEnter()
     {
+        if (shootDestination != null)
+        {
+            transform.position = initialPosition;
+            ballRigidbody.drag = 0;
+            ballRigidbody.angularDrag = 0;
+        }
+        
         Debug.Log("OnCollisionEnter");
         shootDestination = null;
     }
