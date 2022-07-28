930b42473d54c481f39f52eb4b4964726f3db90c
cankayacan <ckayacan@gmail.com>
2022-07-28 12:05:56 +0300

    Revert.

--- Assets/Scripts/Common/Singleton.cs
+++ Assets/Scripts/Common/Singleton.cs
@@ -1,29 +1,18 @@
-using System;
 using UnityEngine;
 
 public class Singleton<T> : MonoBehaviour where T : Component
 {
-    private static bool isAppQuiting;
     private static T instance;
 
     public static T Default
     {
         get
         {
-            if (instance != null)
-            {
-                Debug.Log("Returning existing instance");
-                return instance;
-            }
+            if (instance != null) return instance;
 
             instance = FindObjectOfType<T> ();
-            if (instance != null)
-            {
-                Debug.Log("Found object");
-                return instance;
-            }
+            if (instance != null) return instance;
 
-            Debug.Log("Creating new game object");
             var obj = new GameObject ();
             obj.name = typeof(T).Name;
             instance = obj.AddComponent<T>();
@@ -35,18 +24,16 @@ public class Singleton<T> : MonoBehaviour where T : Component
     {
         if (instance == null)
         {
-            Debug.Log("Awake setting instance");
             instance = this as T;
             DontDestroyOnLoad(gameObject);
         } else
         {
-            Debug.Log("Awake destroying");
             Destroy (gameObject);
         }
     }
 
     private void OnApplicationQuit()
     {
-        isAppQuiting = true;
+        Destroy (gameObject);
     }
 }
