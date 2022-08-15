d3348fa791ec7d2df720e1b06249847cf5e9b317
cankayacan <ckayacan@gmail.com>
2022-08-15 20:11:22 +0300

    Goal camera not turning aroung.

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -6,23 +6,10 @@ public class CameraController: MonoBehaviour
     [SerializeField] private CinemachineVirtualCamera shootingCamera;
     [SerializeField] private CinemachineVirtualCamera goalCamera;
 
-    private void Update()
-    {
-        if (goalCamera.Priority == 1)
-        {
-            MoveGoalCamera();
-        }
-    }
-
     public void Goal()
     {
         goalCamera.Priority = 1;
         shootingCamera.Priority = 0;
-    }
-
-    private void MoveGoalCamera()
-    {
-        goalCamera.transform.Translate(Vector3.right * Time.deltaTime);
     }
 }
 
